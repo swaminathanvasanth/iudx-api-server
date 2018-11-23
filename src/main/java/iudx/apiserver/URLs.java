8ceaf9062ff9ab306a35e6a852c9f0446637824a
Swaminathan Vasanth Rajaraman <swaminathanvasanth.r@gmail.com>
2018-11-23 19:37:41 +0530

    Updating URL configuration file

--- src/main/java/iudx/apiserver/URLs.java
+++ src/main/java/iudx/apiserver/URLs.java
@@ -13,7 +13,24 @@ public class URLs {
 	public static final String broker_password = "admin@123";
 	public static final int broker_port = 5672;
 	public static final String broker_vhost = "/";
-	
+	public static final String psql_database_url = "localhost";
+	public static final String psql_database_username = "postgres";
+	public static final String psql_database_password = "admin";
+	public static final int psql_database_port = 5432;
+	public static final String psql_database_name = "postgres";
+		
+	public static String getPsqlDatabaseUrl() {
+		return psql_database_url;
+	}
+	public static String getPsqlDatabaseUsername() {
+		return psql_database_username;
+	}
+	public static String getPsqlDatabasePassword() {
+		return psql_database_password;
+	}
+	public static int getPsqlDatabasePort() {
+		return psql_database_port;
+	}
 	public static String getBrokerUrl() {
 		return broker_url;
 	}
@@ -29,8 +46,4 @@ public class URLs {
 	public static String getBrokerVhost() {
 		return broker_vhost;
 	}
-	
-	
-	
-
 }
